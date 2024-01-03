4e3ef7a70c027ee71fbf689e2c84c69e641fb564
mistryaksh <mistryaksh1998@gmail.com>
2024-01-03 11:20:47 +0530

    making some changes
    undefined

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Realm from "realm";
+
+const app = new Realm.App({ id: "<Your App ID>" }); // create a new instance of the Realm.App
+async function run() {
+     // login with an anonymous credential
+     await app.logIn(Realm.Credentials.anonymous());
+
+     const DogSchema = {
+          name: "Dog",
+          properties: {
+               _id: "int",
+               name: "string",
+               age: "int",
+          },
+          primaryKey: "_id",
+     };
+
+     const realm = await Realm.open({
+          schema: [DogSchema],
+          sync: {
+               user: app.currentUser,
+               partitionValue: "myPartition",
+          },
+     });
+
+     // The myPartition realm is now synced to the device. You can
+     // access it through the `realm` object returned by `Realm.open()`
+
+     // write to the realm
+}
+run()
+     .then((res) => {
+          console.log("SUCCESS", res);
+     })
+     .catch((err) => {
+          console.error("Failed to open realm:", err);
+     });
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     return (
+          <div className="App">
+               <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                         Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                         Learn React
+                    </a>
+               </header>
+          </div>
+     );
 }
 
 export default App;
